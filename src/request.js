18d7125e1ef8f3da8262c87a617f7564a9b52012
Artem Kodesnikov <artiguard@gmail.com>
2022-10-17 20:45:20 +0300

    node params server

---
+++ src/request.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-console */
+
+import axios from 'axios';
+
+const PORT = process.env.PORT || 3000;
+
+const href = `http://localhost:${PORT}/hello/world/123?x=1&search=some`;
+
+console.log(href);
+
+axios.get(href)
+  .catch(() => console.log('error'));
